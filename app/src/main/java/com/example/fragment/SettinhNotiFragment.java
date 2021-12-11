36ae8334441f7a09ec7a432ddbd8bd877e4d7f29
tramanhsociu <anhltt19411c@st.uel.edu.vn>
2021-12-11 11:30:54 +0700

    update noti setting

---
+++ app/src/main/java/com/example/fragment/SettinhNotiFragment.java
@@ -0,0 +1,31 @@
+package com.example.fragment;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+
+import com.example.onboarding.R;
+import com.example.onboarding.SettingNotificationActivity;
+
+
+public class SettinhNotiFragment extends Fragment {
+
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View view=inflater.inflate(R.layout.fragment_settinh_noti,container,false);
+
+        return view;
+    }
+}
--- app/src/main/java/com/example/onboarding/SettingNotificationActivity.java
+++ app/src/main/java/com/example/onboarding/SettingNotificationActivity.java
@@ -1,6 +1,11 @@
 package com.example.onboarding;
 
+import static androidx.constraintlayout.widget.ConstraintSet.INVISIBLE;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -8,11 +13,17 @@ import android.view.View;
 import android.widget.CompoundButton;
 import android.widget.ImageView;
 import android.widget.Switch;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.example.fragment.SettingNotiFragmentOn;
+import com.example.fragment.SettinhNotiFragment;
 
 public class SettingNotificationActivity extends AppCompatActivity {
     ImageView imvBack;
-    Switch swtNoti;
+    Switch swtNoti, swtBell;
+    TextView txtMyNoti;
+    ConstraintLayout Chuong, MyNoti;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +38,8 @@ public class SettingNotificationActivity extends AppCompatActivity {
     private void linkViews() {
         imvBack=findViewById(R.id.imvBack);
         swtNoti=findViewById(R.id.swtNoti);
+        swtBell=findViewById(R.id.swtBell);
+        txtMyNoti=findViewById(R.id.txtMyNoti);
     }
 
 
@@ -40,12 +53,24 @@ public class SettingNotificationActivity extends AppCompatActivity {
         swtNoti.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
-                if(b){
+                if(compoundButton.isChecked())
+                {
+                    FragmentManager fragmentManager = getSupportFragmentManager();
+                    FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                    fragmentTransaction.replace(R.id.On, new SettingNotiFragmentOn());
+                    fragmentTransaction.commit();
                     Toast.makeText(SettingNotificationActivity.this, "Bật thông báo", Toast.LENGTH_SHORT).show();
                 }else {
+                    FragmentManager fragmentManager = getSupportFragmentManager();
+                    FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                    fragmentTransaction.replace(R.id.On, new SettinhNotiFragment());
+                    fragmentTransaction.commit();
                     Toast.makeText(SettingNotificationActivity.this, "Tắt thông báo", Toast.LENGTH_SHORT).show();
                 }
             }
         });
     }
+
+
+
 }
