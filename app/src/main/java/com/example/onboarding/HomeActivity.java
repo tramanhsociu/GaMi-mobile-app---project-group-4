6bcd1f43777455d9014238483fde681430a524a6
tramanhsociu <anhltt19411c@st.uel.edu.vn>
2021-12-16 22:31:00 +0700

    chat

--- app/src/main/java/com/example/onboarding/HomeActivity.java
+++ app/src/main/java/com/example/onboarding/HomeActivity.java
@@ -10,10 +10,14 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.viewpager2.widget.ViewPager2;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.GridView;
+import android.widget.ImageView;
 
 import com.example.Adapter.BannerAdapter;
 import com.example.Adapter.CategoryAdapter;
@@ -27,21 +31,24 @@ import com.google.android.material.navigation.NavigationBarView;
 import java.util.ArrayList;
 import java.util.List;
 
+import im.crisp.client.ChatActivity;
+import im.crisp.client.Crisp;
 import me.relex.circleindicator.CircleIndicator3;
 
 public class HomeActivity extends AppCompatActivity{
 
     FragmentManager manager;
     BottomNavigationView navigationView;
-
+    ImageView btnChat;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         navigationView = findViewById(R.id.menu_navigation);
-
-
+        btnChat = findViewById(R.id.btnChat);
+        //chat
+        Crisp.configure(getApplicationContext(), "1b4d03b2-db60-4da9-b658-bcf6eceac6f1");
 
        getSupportFragmentManager().beginTransaction()
         .replace(R.id.layout_container,new HomeFragment()).commit();
@@ -68,6 +75,14 @@ public class HomeActivity extends AppCompatActivity{
                }
                getSupportFragmentManager().beginTransaction().replace(R.id.layout_container,fragment).commit();
                return true;
+           }
+       });
+       //nút chat
+       btnChat.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+               Intent crispIntent = new Intent(HomeActivity.this, ChatActivity.class);
+               startActivity(crispIntent);
            }
        });
 
