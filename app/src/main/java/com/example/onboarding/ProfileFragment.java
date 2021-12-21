917c40e44e038805edc030182419031c2f3b0610
ThanhVanNg621 <93310638+ThanhVanNg621@users.noreply.github.com>
2021-12-21 16:36:26 +0700

    fix profile

--- app/src/main/java/com/example/onboarding/ProfileFragment.java
+++ app/src/main/java/com/example/onboarding/ProfileFragment.java
@@ -9,17 +9,20 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 
 public class ProfileFragment extends Fragment{
 
-    ImageButton imbSetting;
+    ImageButton imbSetting, imbSettingProfile, imbGoSettingProfile;
+    TextView txtSettingProfile;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view= inflater.inflate(R.layout.fragment_profile, container, false);
         imbSetting=view.findViewById(R.id.imbSetting);
+        imbSettingProfile=view.findViewById(R.id.imbSettingProfile);
         addEvents();
         return view;
     }
@@ -30,6 +33,13 @@ public class ProfileFragment extends Fragment{
             public void onClick(View view) {
 //                startActivity(new Intent(ProfileFragment, SettingActivity.class));
                 Intent intent = new Intent(getActivity(),SettingActivity.class);
+                getActivity().startActivity(intent);
+            }
+        });
+        imbSettingProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(getActivity(), activity_profile_setting.class);
                 getActivity().startActivity(intent);
             }
         });
